5115c4acc084b644f41067f6fd93876ee3476717
ssray23 <ray.suddhasatta@gmail.com>
2025-06-30 09:22:27 +0100

    Add Flask doc-reviewer app with upload and processing features

---
+++ init.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1QA1uTAcz_N5m4RCYhYgiQxe37j1Vbkk",
+  authDomain: "doc-reviewer-8ac61.firebaseapp.com",
+  projectId: "doc-reviewer-8ac61",
+  storageBucket: "doc-reviewer-8ac61.firebasestorage.app",
+  messagingSenderId: "954581039235",
+  appId: "1:954581039235:web:351b8f4f17949c019a43b6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme'); // Make sure you have this line
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      // This tells Tailwind to use a modern system font stack for all 'font-sans' utilities
+      fontFamily: {
+        sans: ['Helvetica', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
+      },
+    },
+  },
+  plugins: [],
+}
